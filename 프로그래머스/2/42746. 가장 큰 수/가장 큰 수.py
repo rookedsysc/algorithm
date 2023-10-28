a01bc88377c558b7ea80abf16fed55681d8ce164
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-10-29 00:48:55 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,17 +1,22 @@
 from functools import cmp_to_key
-def custom_sort(a, b):
-  if a + b > b + a:
+def custom_sort(str1, str2) : 
+  if str(str1)+str(str2) > str(str2) + str(str1) :
     return -1
-  elif a + b < b + a:
+    
+  elif str(str1)+str(str2) < str(str2) + str(str1) :
     return 1
-  else:
-    return 0 
-def solution(numbers: list) : 
-  numbers = list(map(lambda x: str(x), numbers))
-  if numbers.count('0')==len(numbers):
-    return "0"
-  numbers = sorted(numbers, key=cmp_to_key(custom_sort))
+    
+  else : 
+    return 0
+  
+
+def solution(numbers):
+  numbers = sorted(numbers, key=cmp_to_key(custom_sort)) 
   ans = ""
-  for n in numbers : 
-    ans += f"{n}"
+  
+  for n in numbers :
+    ans += str(n)
+  if ans[0] == "0" : 
+    ans = "0"
+  
   return ans
