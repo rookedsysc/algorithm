4d3f7020a3e10df00856f9e64a5cd9753e26f2f0
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-11-01 04:57:36 +0900

    [level 2] Title: 주식가격, Time: 20.37 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,14 @@
-def solution(prices) :
+def solution(prices):
+  ans = [0 for _ in range(len(prices))]
   stack = []
-  ans = [0] * len(prices) 
   for i in range(len(prices)) :
-    while len(stack) != 0 and prices[stack[-1]] > prices[i] : 
+    while len(stack) > 0 and prices[stack[-1]] > prices[i] :
       cur_i = stack.pop()
-      ans[cur_i] = i - cur_i 
+      ans[cur_i] = i - cur_i
       
     stack.append(i)
     
-  for i in stack : 
-    ans[i] = len(prices) - i - 1
+  for si in stack :
+    ans[si] = len(prices) - 1 - si
     
   return ans
