f168981d904a1cad731523de52150c66f05683a1
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-11-06 15:21:30 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,22 +1,27 @@
 from collections import deque
+
+def calc_location(priorites, location) : 
+  location -= 1 
+  if location < 0 :
+    location = len(priorites) - 1
+  return location
+
 def solution(priorities, location):
   ans = 0
   q = deque(priorities)
-  tar_val = priorities[location]
   
   while q : 
-    cur_max_val = max(q)
+    cur_max = max(q)
     cur_val = q.popleft()
-    location -= 1
-    if cur_max_val == cur_val : 
+    if cur_max == cur_val : 
       ans += 1
-      if location < 0 and cur_val == tar_val : 
-        break
-      elif location < 0 : 
-        location = len(q) + location
-    else : 
-      q.append(cur_val)
-      if location < 0 : 
-        location = len(q) + location
+      if location == 0 :
+        return ans
+      else : 
+        location = calc_location(q, location)
     
+    else :
+      q.append(cur_val)
+      location = calc_location(q, location)
+      
   return ans
