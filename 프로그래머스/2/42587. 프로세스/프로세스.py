957bf159bf36b468b8b70db6aa707254dae653a9
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-11-14 01:52:24 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,3 +1,21 @@
-def solution(priorities, location):
-    answer = 0
-    return answer
+def get_location(priorities:list, location: int) -> int :
+  if location < 0 :
+    location = len(priorities) - 1
+  else :
+    location -= 1
+  return location
+
+def solution(priorities: list, location: int):
+  ans = 0
+  
+  while priorities :
+    max_val = max(priorities)
+    cur_val = priorities.pop(0)
+    location = get_location(priorities, location)
+    if cur_val != max_val :
+      priorities.append(cur_val)
+    else :
+      ans += 1
+      if location < 0 :
+        return ans
+  return ans
