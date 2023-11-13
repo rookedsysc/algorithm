13980edd1d982f551f0072263c7da098d67d1893
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-11-14 01:09:12 +0900

    Time: 34 ms (77.61%), Space: 16.2 MB (32.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,13 @@
+
 class Solution:
   def __init__(self) -> None:
-    self.memory = {}
-  def dfs(self, n:int) -> int :
-    if n == 1 :
+    self.dp_table = {}
+  def climbStairs(self, n: int) -> int:
+    if n == 1 : 
       return 1
     if n == 2 :
       return 2
+    if n not in self.dp_table :
+      self.dp_table[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
     
-    if n not in self.memory :
-      self.memory[n] = self.dfs(n - 1) + self.dfs(n - 2)
-    
-    return self.memory[n]
-    
-  def climbStairs(self, n: int) -> int:
-    return self.dfs(n)
-  
-print(Solution().climbStairs(3))
+    return self.dp_table[n]
