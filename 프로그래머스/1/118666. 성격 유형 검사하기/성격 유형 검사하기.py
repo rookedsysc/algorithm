d4121539c724ab73e3b29593ed0c4f80c996153d
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-11-18 21:43:41 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,18 +1,21 @@
 def solution(survey, choices):
-  mbti_hash = {'R': 0, 'T':0, 'C':0, 'F':0,'J':0,'M':0,'A':0,'N':0}
-  first = ['R','C','J','A']
-  second = ['T','F','M','N']
-  ans = ''
-  for i in range(len(survey)) :
-    if choices[i] < 4 :
-      mbti_hash[survey[i][0]] += 4 - choices[i]
-    elif choices[i] > 4 :
-      mbti_hash[survey[i][1]] += choices[i] - 4
-
-  for i in range(len(first)) :
-    if mbti_hash[first[i]] < mbti_hash[second[i]] :
-      ans += second[i]
-    else :
-      ans += first[i]
-
-  return ans
+    mbti_table = {"R": 0, "T": 0, "C":0,"F":0,"J":0,"M":0,"A":0,"N":0,}
+    ans = ""
+    
+    for i in range(len(survey)) :
+        if choices[i] > 4 :
+            mbti_table[survey[i][1]] += choices[i] - 4
+        elif choices[i] < 4 :
+            mbti_table[survey[i][0]] += 4 - choices[i]
+            
+    first = ['R','C','J','A']
+    second = ['T','F','M','N']
+    print(mbti_table)
+    
+    for i in range(len(first)) :
+        if mbti_table[second[i]] > mbti_table[first[i]] :
+            ans += second[i]
+        else :
+            ans += first[i]
+    
+    return ans 
