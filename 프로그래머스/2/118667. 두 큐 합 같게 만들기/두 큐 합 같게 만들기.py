82f5ff11dba576656c838d03d3aac87664815b65
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-12-04 20:54:55 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 55.59 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,22 +1,25 @@
 from collections import deque
 
 def solution(queue1, queue2):
-    queue1, queue2 = deque(queue1),deque(queue2)
-    count, max_count = 0, len(queue1) * 3
-    sum1, sum2 = sum(queue1), sum(queue2)
-    while count < max_count : 
-        if sum1 > sum2 :
-            cur_val = queue1.popleft()
-            queue2.append(cur_val)
-            sum1 -= cur_val
-            sum2 += cur_val
-        elif sum2 > sum1 :
-            cur_val = queue2.popleft()
-            queue1.append(cur_val)
-            sum1 += cur_val
-            sum2 -= cur_val
-        else :
-            return count 
-        count += 1
+    answer = -2
+    max_count = len(queue1) * 3 
+    count = 0
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    s1, s2 = sum(queue1), sum(queue2)
     
+    while count < max_count :
+        if s1 > s2 :
+            pop_val = queue1.popleft()
+            s1 -= pop_val
+            s2 += pop_val
+            queue2.append(pop_val)
+        elif s1 < s2 :
+            pop_val = queue2.popleft()
+            s1 += pop_val
+            s2 -= pop_val
+            queue1.append(pop_val)
+        else :
+            return count
+        count += 1
     return -1
