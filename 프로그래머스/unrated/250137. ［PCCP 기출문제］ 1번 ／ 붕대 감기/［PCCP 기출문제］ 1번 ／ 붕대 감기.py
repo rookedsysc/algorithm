8f2fd50c682957fa417404b69992b7d98aceb772
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2024-01-07 00:43:32 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.53 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,29 +1,29 @@
+from collections import deque
+
 def solution(bandage, health, attacks):
   max_health = health
+  q = deque(attacks)
   count = 0
-  repeat, sh, ph = bandage[0], bandage[1], bandage[2]
-  attacks_hash = {}
-  for a in attacks:
-    i, d = a[0], a[1]
-    attacks_hash[i] = d
-  keys = attacks_hash.keys()
-  for i in range(attacks[-1][0] + 1):
-    if i in keys:  # 이번 차수 공격이 있는 경우
-      health -= attacks_hash[i]
-      if health < 1:  # 체력 0이하시 사망
+  game_round = 0
+
+  def plushealth(plus):
+    if health + plus > max_health:
+      return max_health
+    return health + plus
+
+  while q:
+    if game_round == q[0][0]:
+      _, b = q.popleft()
+      health -= b
+      count = 0
+      if health <= 0:
         return -1
-      else:
-        count = 0
     else:
       count += 1
-      if count == repeat:
-        health = health + sh + ph  # 추가 체력 줌
-        count = 0
-        if health > max_health:
-          health = max_health
+      if count != bandage[0]:
+        health = plushealth(bandage[1])
       else:
-        health += sh
-        if health > max_health:
-          health = max_health
+        count = 0
+        health = plushealth(bandage[2] + bandage[1])
+    game_round += 1
   return health
-
