6eadf425e8af735a0687fe9d79f538e523da7872
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2024-04-15 10:29:54 +0900

    Time: 7 ms (64.86%), Space: 47.6 MB (74.36%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> results = new HashMap<>();
+
+        for (String s : strs) {
+            // 문자열을 문자 배열로 변환
+            char[] chars = s.toCharArray();
+            // 문자 배열 정렬
+            Arrays.sort(chars);
+            // 문자 배열을 키로 하기 위해 다시 문자열로 변환
+            String key = String.valueOf(chars);
+
+            // 만약 기존에 없던 키라면 빈 리스트를 삽입
+            if (!results.containsKey(key)) {
+                results.put(key, new ArrayList<>());
+            }
+
+            results.get(key)
+                    .add(s);
+        }
+
+        return new ArrayList<>(results.values());
+    }
+}
