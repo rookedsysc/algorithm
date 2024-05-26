472bdf12dc9399018b5440256af75d5420a72d85
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2024-05-26 15:47:25 +0900

    [level 1] Title: 모의고사, Time: 3.55 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,30 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] members = {0,0,0};
-        int[][] picks = {{1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-        for(int i = 0; i < answers.length; i ++) {
-            if(picks[0][i % 5] == answers[i]) {
-                members[0] ++;
-            }
-
-            if(picks[1][i % 8] == answers[i]) {
-                members[1] ++;
-            }
-
-            if(picks[2][i % 10] == answers[i]) {
-                members[2] ++;
-            }
-		}
-        int max = Arrays.stream(members).max().getAsInt();
-        ArrayList<Integer> tmp = new ArrayList<Integer>();
-        for(int i = 0; i < members.length; i++) {
-            if(members[i] == max) {
-                tmp.add(i + 1);
+        int[][] randoms = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] points = new int[3];
+        
+        for(int i = 0; i < answers.length; i++) {
+            if(randoms[0][i % 5] == answers[i]) {
+                points[0] ++;
+            } 
+            if (randoms[1][i % 8] == answers[i]) {
+                points[1] ++;
+            } 
+            if(randoms[2][i % 10] == answers[i]){
+                points[2] ++;
             }
         }
-        int[] ans = tmp.stream().mapToInt(Integer::intValue).toArray();
-
-        return ans;
+        ArrayList<Integer> highScores = new ArrayList<>();
+        int max = Arrays.stream(points).max().getAsInt();
+        for(int i = 0; i < points.length; i++) {
+            if(points[i] == max) {
+                highScores.add(i + 1);
+            }
+        }
+        return highScores.stream().mapToInt(Integer::intValue).toArray();
     }
 }
