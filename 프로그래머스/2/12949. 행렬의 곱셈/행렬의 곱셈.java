444760185ff6207c2cdaea96d458055ecfdd93c8
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2024-06-07 15:14:34 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.50 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
+        public int[][] solution(int[][] arr1, int[][] arr2) {
         int Y = arr1.length;
         int X = arr2[0].length;
-        int[][] ans = new int[Y][X];
-        
-        for(int y = 0; y < Y; y++) {
+        int[][] answer = new int[Y][X];
+        for(int y = 0; y < Y; y ++) {
+            // [0][0] * []
             for(int x = 0; x < X; x ++) {
-                for(int k = 0; k < arr1[0].length; k++) {
-                    ans[y][x] += arr1[y][k] * arr2[k][x];
+                for(int k = 0; k < arr2.length; k ++) {
+                    answer[y][x] += arr2[k][x] * arr1[y][k];
                 }
             }
         }
-            
-        return ans;
+        return answer;
     }
+
 }
