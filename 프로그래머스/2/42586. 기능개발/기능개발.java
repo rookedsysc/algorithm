d6b778cf7d9a2a791dc2ffab6c3b618d0976560f
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2024-06-19 00:43:02 +0900

    [level 2] Title: 기능개발, Time: 2.04 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,25 +1,33 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int len = progresses.length;
-        int[] daysLeft = new int[len];
-        ArrayDeque<Integer> answer = new ArrayDeque<>();
-        for(int i = 0; i < len; i ++) {
-            daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-        }
-        int count = 0;
-        int maxDay = daysLeft[0];
-        for(int i = 0; i < len ; i++) {
-            if(daysLeft[i] <= maxDay) {
-				count++;
-            } else {
-                answer.addLast(count);
-                count = 1;
-                maxDay = daysLeft[i];
+        ArrayList<Integer> result = new ArrayList<>();
+        int cnt = 0; 
+        int times = 0;
+        int idx = 0;
+        int pLen = progresses.length;
+        int last = -1;
+        while(idx < pLen) {
+            times = (100 - progresses[idx]) / speeds[idx]; // 지금 현재의 idx가 100 이상되기 위해서 필요한 시간
+            if((100 - progresses[idx]) % speeds[idx] != 0) {
+                times ++; // 딱 나눠 떨어지지 않는다면 cnt 1 늘림
+            } 
+            for(int i = idx; i < pLen; i ++) {
+                progresses[i] += speeds[i] * times;
+                if(progresses[i] >= 100) {
+                    if(last == -1) {
+                    	idx = i + 1;
+                    	cnt ++;
+                    }
+				} else {
+                    last = i;
+                }
             }
-        }
-        answer.addLast(count);
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+            result.add(cnt);
+            cnt = 0;
+            last = -1;
+		}
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
