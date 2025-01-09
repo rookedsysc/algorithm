0a480653a7a76269f5a48711d6196783458a6521
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2025-01-09 20:31:01 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 696 ms, Memory: 227328 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -2,29 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static String str;
-    static Set<String> comb = new HashSet<>();
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws Exception {
-        st = new StringTokenizer(br.readLine());
-        str = st.nextToken();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i = 0; i < str.length(); i ++) {
-            StringBuilder sb = new StringBuilder();
-            makeComb(sb, i);
+        String str = br.readLine();
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                set.add(str.substring(i, j));
+            }
         }
-        System.out.println(comb.size());
-    }
 
-    private static void makeComb(StringBuilder sb, int idx) {
-        if(idx >= str.length()) {
-            return;
-        }
-        sb.append(str.charAt(idx));
-        comb.add(sb.toString());
-        makeComb(new StringBuilder(sb), idx + 1);
+        System.out.println(set.size());
     }
 }
