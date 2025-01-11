d3cada00a18890f6d42f14173e9903a86de9138a
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2025-01-12 00:32:45 +0900

    Time: 9 ms (15.8%), Space: 74.7 MB (47.1%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.java
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -16,6 +16,9 @@ class Solution {
             }
         }
 
+        System.out.println((int)'a');
+        System.out.println((int)'z');
+
         for (int i = 1; i < adds.length; i++) {
             adds[i] += adds[i - 1];
         }
@@ -24,8 +27,9 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             int shift = adds[i] % 26; 
             // shift가 양수 또는 음수 일 수 있기 때문에 26을 더하고 26%으로 나눈 후 다시 'a'를 더함으로써 자리를 찾음
-            // s.charAt(i)가 a고 shift가 -25라면 (s.charAt(i) - 'a' + shift + 26)의 결과는 1이 됨
-            char c = (char) ((s.charAt(i) - 'a' + shift + 26) % 26 + 'a'); // wrap-around 처리
+            // s.charAt(i)가 z고 shift가 -25라면 (s.charAt(i) - 'a' + shift + 26)의 결과는 26이 됨
+            // 26인 경우 0이 되야 하므로 %26을 해주는 것임
+            char c = (char) ((s.charAt(i) - 'a' + shift + 26) % 26 + 'a'); 
             sb.append(c);
         }
 
